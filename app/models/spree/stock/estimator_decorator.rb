79741b6cd90bda8ad78b376019b0cfddd027c27e
Kyle Van Wagenen <kvanwagenen@gmail.com>
2016-02-22 15:11:38 -0700

    Automatically select cheapest shipping rate after calculating for package

--- app/models/spree/stock/estimator_decorator.rb
+++ app/models/spree/stock/estimator_decorator.rb
@@ -3,7 +3,9 @@ Spree::Stock::Estimator.class_eval do
   private
 
   def calculate_shipping_rates(package, ui_filter)
-    shipping_rates_builder(package).shipping_rates
+    rates = shipping_rates_builder(package).shipping_rates
+    choose_default_shipping_rate(rates)
+    rates
   end
 
   def shipping_rates_builder(package)
